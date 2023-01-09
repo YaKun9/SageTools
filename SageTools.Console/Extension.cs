497c42dfbe80fb89e07824f1968f7a871d824192
劉亞坤 <13140659949@163.com>
2023-01-09 13:47:22 +0800

    更新发布程序

---
+++ SageTools.Console/Extension.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using SageTools.Extension;
+
+namespace SageTools.Console;
+
+public static class Extension
+{
+    public static string GetArgValue(this string[] args, string argKey)
+    {
+        if (args.IsNullOrEmpty() || argKey.IsNullOrWhiteSpace()) return default;
+        argKey = '-' + argKey.TrimStart('-').TrimEnd('=') + '=';
+        return args.Where(x => x.Contains(argKey)).Select(x => x.Replace(argKey, "")).FirstOrDefault();
+    }
+}
--- SageTools.Console/Program.cs
+++ SageTools.Console/Program.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.IO;
-using SageTools.Extension;
 using System.Linq;
 using System.Xml;
+using SageTools.Extension;
 
 namespace SageTools.Console
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var help = args.Any(x => x.Contains("-h"));
             if (help)
@@ -17,42 +17,45 @@ namespace SageTools.Console
                 Environment.Exit(0);
             }
             System.Console.WriteLine("处理xml文件");
-            var source = args.Where(x => x.Contains("-source=")).Select(x => x.Replace("-source=", "")).FirstOrDefault();
-            var step = args.Where(x => x.Contains("-step=")).Select(x => x.Replace("-step=", "")).FirstOrDefault();
-            var version =  args.Where(x => x.Contains("-version=")).Select(x => x.Replace("-version=", "")).FirstOrDefault();
-            if (source.IsNullOrEmpty())
-            {
-                System.Console.WriteLine("执行错误：必须指定 -source");
-                Environment.Exit(-1);
-            }
+            var source = args.GetArgValue("source");
+            var step = args.GetArgValue("step");
+            var version = args.GetArgValue("version");
+
             var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, source);
             if (!File.Exists(path))
             {
                 System.Console.WriteLine($"执行错误：【{path}】文件不存在");
                 Environment.Exit(-1);
             }
-            var xmlDoc = new XmlDocument(); //新建XML文件
-            xmlDoc.Load(path); //加载XML文件
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(path);
             var versionNode = xmlDoc.DocumentElement?.SelectSingleNode("PropertyGroup")?.SelectSingleNode("Version");
-            if (versionNode == null)
+            if (versionNode.IsNull())
             {
                 System.Console.WriteLine("执行错误：项目没有配置版本号");
                 Environment.Exit(-1);
             }
             if (version.IsNullOrEmpty())
             {
-                version = versionNode.InnerText;
-                var lastNum = version.Substring(version.LastIndexOf(".", StringComparison.Ordinal) + 1);
+                version = versionNode!.InnerText;
+                var lastNum = version[(version.LastIndexOf(".", StringComparison.Ordinal) + 1)..];
                 var newNum = lastNum.ToInt32() + step;
-                var newVersion = version.Substring(0, version.LastIndexOf(".", StringComparison.Ordinal) + 1) + newNum;
+                var newVersion = version[..(version.LastIndexOf(".", StringComparison.Ordinal) + 1)] + newNum;
                 versionNode.InnerText = newVersion;
                 System.Console.WriteLine($"当前版本{version}，末尾版本号{lastNum}，新版本号{newVersion}");
             }
             else
             {
                 System.Console.WriteLine($"直接指定新版本号为 {version}");
-                versionNode.InnerText = version;
+                versionNode!.InnerText = version;
             }
+
+            var copyright = xmlDoc.DocumentElement?.SelectSingleNode("PropertyGroup")?.SelectSingleNode("Copyright");
+            if (copyright.IsNotNull())
+            {
+                copyright!.InnerText = $"Copyright © LiuDanK 2021-{DateTime.Now.Year}";
+            }
+
             xmlDoc.Save(path);
             System.Console.WriteLine("处理完毕!");
         }
